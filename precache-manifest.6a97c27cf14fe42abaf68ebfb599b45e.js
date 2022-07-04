8b43554c993f87cfd78bb8c69a35e71b3d6b3365
wafa-alrizqi <wafa.alrizqi@gmail.com>
2022-07-04 09:51:22 +0300

    Updates

---
+++ precache-manifest.6a97c27cf14fe42abaf68ebfb599b45e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "766091ef4f8a7d07b012",
+    "url": "/react-to-do-app/static/css/main.80add2dd.chunk.css"
+  },
+  {
+    "revision": "766091ef4f8a7d07b012",
+    "url": "/react-to-do-app/static/js/main.09ac2c7f.chunk.js"
+  },
+  {
+    "revision": "b9d6fa0954b7b9515672",
+    "url": "/react-to-do-app/static/js/runtime~main.952ab04e.js"
+  },
+  {
+    "revision": "e1e9a16cd4d9d9411ec9",
+    "url": "/react-to-do-app/static/js/2.08cba4b5.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/react-to-do-app/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "f4833a6c7a5350a0f5c52e8ef9f2ad4a",
+    "url": "/react-to-do-app/index.html"
+  }
+];
